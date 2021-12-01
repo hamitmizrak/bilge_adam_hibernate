ebc07c7b8ddbcb03d96ec0ec87939cb708aa21aa
hamitmizrak <hamitmizrak@gmail.com>
2021-12-01 11:24:01 +0300

    hibernate aggreagete like

--- src/main/java/com/bilgeadam/hql/_10_like.java
+++ src/main/java/com/bilgeadam/hql/_10_like.java
@@ -1,11 +1,17 @@
 package com.bilgeadam.hql;
 
+import java.util.ArrayList;
+
+import javax.persistence.TypedQuery;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
+import com.bilgeadam.entity.StudentEntity;
 import com.bilgeadam.util.HibernateUtil;
 
+// merge=update find=find delete=remove insert=persist select=TypedQuery
 public class _10_like {
 	// logger
 	private static final Logger logger = LogManager.getLogger(_10_like.class);
@@ -14,21 +20,21 @@ public class _10_like {
 		// Session
 		Session session = HibernateUtil.getSessionfactory().openSession();
 		
-		// String hql = "select stu from StudentEntity as stu where tcNumber between
-		// :minKey and :maxKey";
-		//
-		// int minValue = 4, maxValue = 7;
-		// TypedQuery<StudentEntity> typedQuery = session.createQuery(hql,
-		// StudentEntity.class);
-		// typedQuery.setParameter("minKey", minValue);
-		// typedQuery.setParameter("maxKey", maxValue);
-		//
-		// ArrayList<StudentEntity> studentEntities = (ArrayList<StudentEntity>)
-		// typedQuery.getResultList();
-		//
-		// for (StudentEntity temp : studentEntities) {
-		// logger.info(temp);
-		// }
+		// sql
+		// select * from student as st where st.student_surname like '%0';
+		
+		// "select stu from StudentEntity as stu where stu.studentSurname like '%0'";
+		// "select stu from StudentEntity as stu where stu.studentSurname like '0%'";
+		// "select stu from StudentEntity as stu where stu.studentSurname like '%0%'";
+		
+		String hql = "select stu from StudentEntity as stu where stu.studentSurname like '%0'";
+		
+		TypedQuery<StudentEntity> typedQuery = session.createQuery(hql, StudentEntity.class);
+		ArrayList<StudentEntity> studentEntities = (ArrayList<StudentEntity>) typedQuery.getResultList();
+		
+		for (StudentEntity temp : studentEntities) {
+			logger.info(temp);
+		}
 		
 	}
 }
